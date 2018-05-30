49c7df76f5c45df4bf681381b1c1a26d59eebe7d
Patrick Hennis <patrickhennis@gmail.com>
2018-05-30 14:55:19 -0400

    updated readme and metrics.sh

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # metrics
+
+## use
+to gather data for metrics, run `metrics.sh` which contains two functions,
+`mempool()` and `send()`. `mempool()` collects transaction id's and time from the
+mempool. `send()` sends transactions containing inputs of `0.0001` in amount of
+`0.0001`, `0.0049`, and `0.0199`. `mempool()` runs every second checking for new
+transactions in the mempool and `send()` runs once per hour.
+
+after running `metrics.sh` for a while, two text files will be created, `metrics.txt`
+and `mempool.txt`. `metrics.py` is called from `metrics.sh` and takes the data
+from both files and and retrieves data from the insight api and creates `data.csv`
+
+## setup
+in the `send()` function in `metrics.sh` the `zfrom` and `zto` addresses need to
+be set accordingly
--- metrics.sh
+++ metrics.sh
@@ -1,69 +1,106 @@
 #!/bin/bash
 
-zfrom="ztftydJRhDSXvctekQB7e1BRubfhnFWFdgS"
-zto="ztcLKgUC9JrN1519ZjXmNy9wXAKnVcFE9tX"
-ztwo="0.0001"
-zfifty="0.0049"
-zmany="0.0199"
+mempool () { # function to scrape transactions from mempool
+  memcall="$(/Applications/ZENCashWallet.app/Contents/Java/zen-cli getrawmempool)"
+  time="$(date +%s)"
+  while IFS=',' read -ra ADDR; do
+       for i in "${ADDR[@]}"; do
+           if [[ $i == *"\""* ]]; then
+             if ! grep -q "${i:3:64}" mempool.txt; then
+               echo "${i:3:64}, $time">> mempool.txt
+             fi
+           fi
+       done
+  done <<< "$memcall"
+}
 
 
-sendtwo="$(zen-cli z_sendmany "$zfrom" "[{\"amount\": $ztwo, \"address\": \"$zto\"}]")"
-sendfifty="$(zen-cli z_sendmany "$zfrom" "[{\"amount\": $zfifty, \"address\": \"$zto\"}]")"
-sendmany="$(zen-cli z_sendmany "$zfrom" "[{\"amount\": $zmany, \"address\": \"$zto\"}]")"
+send () { # function to create transactions with multiple inputs
+  zfrom="ztftydJRhDSXvctekQB7e1BRubfhnFWFdgS"
+  zto="ztcLKgUC9JrN1519ZjXmNy9wXAKnVcFE9tX"
+  ztwo="0.0001"
+  zfifty="0.0049"
+  zmany="0.0199"
 
-success="\"status\": \"success\","
 
-echo
-if [[ $sendtwo = *opid* ]]; then
-  opr="$(zen-cli z_getoperationresult)"
-  if [[ "$opr" == *"$success"* ]]; then
-    echo "$opr" >> metrics.txt
-    echo "two success"
+  sendtwo="$(zen-cli z_sendmany "$zfrom" "[{\"amount\": $ztwo, \"address\": \"$zto\"}]")"
+  sendfifty="$(zen-cli z_sendmany "$zfrom" "[{\"amount\": $zfifty, \"address\": \"$zto\"}]")"
+  sendmany="$(zen-cli z_sendmany "$zfrom" "[{\"amount\": $zmany, \"address\": \"$zto\"}]")"
+
+  success="\"status\": \"success\","
+
+  echo
+  if [[ $sendtwo = *opid* ]]; then
+    opr="$(zen-cli z_getoperationresult)"
+    if [[ "$opr" == *"$success"* ]]; then
+      echo "$opr" >> metrics.txt
+      echo "two success"
+    else
+      echo
+      echo "-------------"
+      echo "failed"
+      echo "$opr"
+      echo "-------------"
+      echo
+    fi
   else
-    echo
-    echo "-------------"
-    echo "failed"
-    echo "$opr"
-    echo "-------------"
-    echo
+    echo "transaction malformed"
   fi
-else
-  echo "transaction malformed"
-fi
-echo
-echo
-if [[ $sendfifty = *opid* ]]; then
-  opr="$(zen-cli z_getoperationresult)"
-  if [[ "$opr" == *"$success"* ]]; then
-    echo "$opr" >> metrics.txt
-    echo "fifty success"
+  echo
+  echo
+  if [[ $sendfifty = *opid* ]]; then
+    opr="$(zen-cli z_getoperationresult)"
+    if [[ "$opr" == *"$success"* ]]; then
+      echo "$opr" >> metrics.txt
+      echo "fifty success"
+    else
+      echo
+      echo "-------------"
+      echo "failed"
+      echo "$opr"
+      echo "-------------"
+      echo
+    fi
   else
-    echo
-    echo "-------------"
-    echo "failed"
-    echo "$opr"
-    echo "-------------"
-    echo
+    echo "transaction malformed"
   fi
-else
-  echo "transaction malformed"
-fi
-echo
-echo
-if [[ $sendmany = *opid* ]]; then
-  opr="$(zen-cli z_getoperationresult)"
-  if [[ "$opr" == *"$success"* ]]; then
-    echo "$opr" >> metrics.txt
-    echo "many success"
+  echo
+  echo
+  if [[ $sendmany = *opid* ]]; then
+    opr="$(zen-cli z_getoperationresult)"
+    if [[ "$opr" == *"$success"* ]]; then
+      echo "$opr" >> metrics.txt
+      echo "many success"
+    else
+      echo
+      echo "-------------"
+      echo "failed"
+      echo "$opr"
+      echo "-------------"
+      echo
+    fi
   else
-    echo
-    echo "-------------"
-    echo "failed"
-    echo "$opr"
-    echo "-------------"
-    echo
+    echo "transaction malformed"
   fi
-else
-  echo "transaction malformed"
-fi
-echo
+  echo
+}
+
+
+
+starthour="$(date +%H)"
+while :
+do
+  if [[ $starthour == $(date +%H) ]]; then # check to see if same hour as when started
+    # runs constantly throughout hour
+    mempool
+
+    sleep 1
+  else # update time
+    # runs hourly
+    echo "$(python3 metrics.py)"
+    send
+
+    starthour="$(date +%H)"
+    sleep 1
+  fi
+done
