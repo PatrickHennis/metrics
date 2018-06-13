7b5316c5269096e5ca6dafe5d38613883ea6a167
Patrick Hennis <patrickhennis@gmail.com>
2018-06-12 21:56:36 -0400

    table can sort

--- web/index.py
+++ web/index.py
@@ -6,7 +6,7 @@ def connect():
     connection = MongoClient('localhost', 27017)
     return connection
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 handle = connect()
 
 @app.route("/index" ,methods=['GET'])
@@ -21,4 +21,4 @@ def index():
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
 
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port)
