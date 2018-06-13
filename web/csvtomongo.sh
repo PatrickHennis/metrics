9f41a2357b0bafe38d3cc6a4646db05477b8c1c4
Patrick Hennis <patrickhennis@gmail.com>
2018-06-13 15:55:16 -0400

    improved mongo to csv script

--- web/csvtomongo.sh
+++ web/csvtomongo.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-echo "$(mongoimport --db data --collection txs --type csv --file '../data.csv' --numInsertionWorkers 5)"
+echo "$(mongoimport --db data --collection txs --type csv --file '../metrics.csv' --headerline --numInsertionWorkers 5)"
+echo "txid,amount,creation_time,inclusion_time,delta,number_inputs,number_outputs" > ../metrics.csv
